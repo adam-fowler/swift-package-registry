1d416129cd9c41ee231787d86d812b9b3c20c337
Adam Fowler <adamfowler71@gmail.com>
2024-02-08 12:52:56 +0000

    Re-ordering router

--- Sources/PackageRegistry/Controllers/PackageRegistryController.swift
+++ Sources/PackageRegistry/Controllers/PackageRegistryController.swift
@@ -8,13 +8,13 @@ extension HTTPField.Name {
 struct PacakageRegistryController {
     typealias Context = RequestContext
     func addRoutes(to router: HBRouter<Context>) {
-        router.on("**", method: .options, use: options)
         router.get("/{scope}/{name}", use: list)
         router.get("/{scope}/{name}/{version}", use: getMetadata)
         router.get("/{scope}/{name}/{version}/Package.swift{swiftVersion}", use: getMetadataForSwiftVersion)
         router.get("/{scope}/{name}/{version}.zip", use: download)
         router.get("/identifiers{url}", use: lookupIdentifiers)
-        router.put("/{scope}/{name}", use: createRelease)
+        router.put("/{scope}/{name}/{version}", use: createRelease)
+        router.on("**", method: .options, use: options)
     }
 
     @Sendable func options(_ request: HBRequest, context: Context) async throws -> HBResponse {
