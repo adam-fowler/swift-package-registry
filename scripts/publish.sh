1bc94d68ba96694065e206081ec4605aee867470
Adam Fowler <adamfowler71@gmail.com>
2025-01-17 10:28:00 +0000

    Update publish to not use signing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -97,7 +97,9 @@ PACKAGE_ID="$OWNER_NAME.$PACKAGE_NAME"
 echo "Publishing $PACKAGE_ID v$VERSION ..."
 
 swift package-registry publish $PACKAGE_ID $VERSION \
-    --metadata-path "$PACKAGE_METADATA_FILE" \
-    --private-key-path "$SCRIPT_FOLDER/../temp/private-key.der" \
-    --cert-chain-paths "$SCRIPT_FOLDER/../temp/cert.der"
+    --metadata-path "$PACKAGE_METADATA_FILE"
+#swift package-registry publish $PACKAGE_ID $VERSION \
+#    --metadata-path "$PACKAGE_METADATA_FILE" \
+#    --private-key-path "$SCRIPT_FOLDER/../temp/private-key.der" \
+#    --cert-chain-paths "$SCRIPT_FOLDER/../temp/cert.der"
     
